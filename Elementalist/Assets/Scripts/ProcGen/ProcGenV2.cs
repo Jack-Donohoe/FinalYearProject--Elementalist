4bb43b13d496d8942c087c6e606bb3c161d56ee6
Jack Donohoe <20093738@mail.wit.ie>
2024-02-14 20:24:07 +0000

    Proc Gen V2 Complete

--- Elementalist/Assets/Scripts/ProcGen/ProcGenV2.cs
+++ Elementalist/Assets/Scripts/ProcGen/ProcGenV2.cs
@@ -5,82 +5,184 @@ using UnityEngine;
 public class ProcGenV2 : MonoBehaviour
 {
     public int levelSize;
-    public int roomAmount;
-    
+
+    public GameObject startRoom, endRoom;
     public GameObject[] emptyRooms;
     public GameObject[] enemyRooms;
 
     public double enemyFrequency;
 
     private int[,] level;
-    
+
+    private Vector3 startPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        level = new int[levelSize,levelSize];
-        
+        level = new int[levelSize, levelSize];
+
         GenerateMap();
-    }
 
-    List<(int,int)> PlaceRooms()
-    {
-        List<(int, int)> roomLocations = new List<(int, int)>();
-        
-        int rand = Random.Range(0, levelSize - 1);
-
-        level[levelSize - 1, rand] = 1; 
-        
-        rand = Random.Range(0, levelSize - 1);
-
-        level[0, rand] = 2;
-        
-        for (int i = 0; i < roomAmount; i++)
-        {
-            int x = Random.Range(0, levelSize - 1);
-            int z = Random.Range(0, levelSize - 1);
-
-            if (level[x, z] == 0)
-            {
-                level[x, z] = 3;
-            }
-            else
-            {
-                x = Random.Range(0, levelSize - 1);
-                z = Random.Range(0, levelSize - 1);
-
-                level[x, z] = 3;
-            }
-
-            Debug.Log("X: " + x + " Z: " + z);
-            roomLocations.Add((x,z));
-        }
-
-        return roomLocations;
-    }
-
-    void CreateHallways(List<(int, int)> roomLocations)
-    {
-        List<List<(int, int)>> paths = new List<List<(int, int)>>();
+        GameObject.Find("Player").transform.position = startPos;
     }
 
     void GenerateMap()
     {
-        List<(int, int)> roomLocations = PlaceRooms();
-        
-        for (int i = 0; i < levelSize; i++)
+        int rand_position = Random.Range(1, levelSize - 2);
+
+        level[levelSize - 1, rand_position] = 3;
+
+        rand_position = Random.Range(1, levelSize - 2);
+
+        level[0, rand_position] = 4;
+
+        for (int x = 0; x < levelSize; x++)
         {
-            for (int j = 0; j < levelSize; j++)
+            for (int z = 0; z < levelSize; z++)
             {
-                if (level[i, j] == 1 || level[i, j] == 2)
+                if (level[x, z] == 0)
                 {
-                    GameObject room = (Instantiate(emptyRooms[0], new Vector3(i * 20f, 0f, j * 20f), Quaternion.identity, this.transform));
-                } else if (level[i, j] == 3)
-                {
-                    GameObject room = (Instantiate(emptyRooms[0], new Vector3(i * 20f, 0f, j * 20f), Quaternion.identity, this.transform));
+                    double rand = Random.Range(0f, 1.0f);
+
+                    if (rand < enemyFrequency)
+                    {
+                        level[x, z] = 2;
+                    }
+                    else
+                    {
+                        level[x, z] = 1;
+                    }
                 }
             }
         }
-        
-        CreateHallways(roomLocations);
+
+        for (int x = 0; x < levelSize; x++)
+        {
+            for (int z = 0; z < levelSize; z++)
+            {
+                if (level[x, z] == 1)
+                {
+                    if (x == 0 && z == 0)
+                    {
+                        GameObject room = (Instantiate(emptyRooms[2], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                        room.transform.Rotate(0f, 90f, 0f);
+                    }
+                    else if (x == 0 && z == levelSize - 1)
+                    {
+                        GameObject room = (Instantiate(emptyRooms[2], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                        room.transform.Rotate(0f, 180f, 0f);
+                    }
+                    else if (x == levelSize - 1 && z == 0)
+                    {
+                        GameObject room = (Instantiate(emptyRooms[2], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                    }
+                    else if (x == levelSize - 1 && z == levelSize - 1)
+                    {
+                        GameObject room = (Instantiate(emptyRooms[2], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                        room.transform.Rotate(0f, -90f, 0f);
+                    }
+                    else if (x == 0)
+                    {
+                        GameObject room = (Instantiate(emptyRooms[1], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                        room.transform.Rotate(0f, 90f, 0f);
+                    }
+                    else if (x == levelSize - 1)
+                    {
+                        GameObject room = (Instantiate(emptyRooms[1], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                        room.transform.Rotate(0f, -90f, 0f);
+                    }
+                    else if (z == 0)
+                    {
+                        GameObject room = (Instantiate(emptyRooms[1], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                    }
+                    else if (z == levelSize - 1)
+                    {
+                        GameObject room = (Instantiate(emptyRooms[1], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                        room.transform.Rotate(0f, 180f, 0f);
+                    }
+                    else
+                    {
+                        GameObject room = (Instantiate(emptyRooms[0], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                    }
+                }
+                else if (level[x, z] == 2)
+                {
+                    if (x == 0 && z == 0)
+                    {
+                        GameObject room = (Instantiate(enemyRooms[2], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                        room.transform.Rotate(0f, 90f, 0f);
+                    }
+                    else if (x == 0 && z == levelSize - 1)
+                    {
+                        GameObject room = (Instantiate(enemyRooms[2], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                        room.transform.Rotate(0f, 180f, 0f);
+                    }
+                    else if (x == levelSize - 1 && z == 0)
+                    {
+                        GameObject room = (Instantiate(enemyRooms[2], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                    }
+                    else if (x == levelSize - 1 && z == levelSize - 1)
+                    {
+                        GameObject room = (Instantiate(enemyRooms[2], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                        room.transform.Rotate(0f, -90f, 0f);
+                    }
+                    else if (x == 0)
+                    {
+                        GameObject room = (Instantiate(enemyRooms[1], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                        room.transform.Rotate(0f, 90f, 0f);
+                    }
+                    else if (x == levelSize - 1)
+                    {
+                        GameObject room = (Instantiate(enemyRooms[1], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                        room.transform.Rotate(0f, -90f, 0f);
+                    }
+                    else if (z == 0)
+                    {
+                        GameObject room = (Instantiate(enemyRooms[1], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                    }
+                    else if (z == levelSize - 1)
+                    {
+                        GameObject room = (Instantiate(enemyRooms[1], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                        room.transform.Rotate(0f, 180f, 0f);
+                    }
+                    else
+                    {
+                        GameObject room = (Instantiate(enemyRooms[0], new Vector3(x * 20f, 0f, z * 20f),
+                            Quaternion.identity, this.transform));
+                    }
+                } 
+                else if (level[x, z] == 3)
+                {
+                    GameObject room = (Instantiate(startRoom, new Vector3(x * 20f, 0f, z * 20f),
+                        Quaternion.identity, this.transform));
+                    room.transform.Rotate(0f, -90f, 0f);
+                    
+                    startPos = new Vector3(room.transform.position.x, 1.5f, room.transform.position.z);
+                }
+                else if (level[x, z] == 4)
+                {
+                    GameObject room = (Instantiate(endRoom, new Vector3(x * 20f, 0f, z * 20f),
+                        Quaternion.identity, this.transform));
+                    room.transform.Rotate(0f, 90f, 0f);
+                }
+            }
+        }
     }
 }
