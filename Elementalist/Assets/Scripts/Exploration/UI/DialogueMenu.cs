1175f4730e23f9e0cab052c4ff4817cb2b761574
Jack Donohoe <20093738@mail.wit.ie>
2024-04-17 20:04:18 +0100

    Tutorial Messages Added, Final Playtest Build Complete

--- Elementalist/Assets/Scripts/Exploration/UI/DialogueMenu.cs
+++ Elementalist/Assets/Scripts/Exploration/UI/DialogueMenu.cs
@@ -9,28 +9,26 @@ public class DialogueMenu : MonoBehaviour
     
     public TMP_Text dialogueText;
 
+    public GameObject inventoryTutorialMenu;
+    public TMP_Text inventoryText;
+
     private string tutorial = 
         "Hello! Welcome to Elementalist." +
         "\n\nThe goal in this game is to defeat enemies, grow in strength and find and combine new elements in order to defeat each of the three floor guardians blocking the way. " +
-        "\n\nIf you do this, you'll complete the game! So go forth brave adventurer, and good luck!";
+        "\n\nIf you do this, you'll complete the game! So go forth and good luck!";
 
     private string inventorytutorial =
         "This is your inventory menu. It contains three separate tabs, which you can navigate between with the arrow buttons located at the top of each tab. " +
-        "\n\nThe first is the player info tab, which shows information about the player's level and XP (how close they are to levelling up), current health and magic (HP and MP respectively) and the player's current statistics or stats." +
+        "\n\nThe first is the player info tab, which shows information about the player's level and XP, current health and magic and the player's current stats." +
         "\n\nThe second is the element inventory, which shows the up to four elements you currently possess. Elements can be removed down to a minimum of one, and you need to have an empty slot in order to gain a new element from a defeated enemy." +
-        "\n\nThe final tab is the element combination menu. This menu allows you to combine two of the base elements of Fire,Water,Earth and Air into a new combination element which will be more powerful, but cannot be combined again. " +
-        "Combining two elements will use them up and you will need to reacquire them in order to use them again, so be mindful.";
+        "\n\nThe final tab is the element combination menu. This menu allows you to combine two of the base elements of Fire,Water,Earth and Air into a new combination element. " +
+        "Combining two elements will use them up and you will need to reacquire them in order to use them again.";
     
     // Start is called before the first frame update
     void Start()
     {
         dialogueText.text = tutorial;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        inventoryText.text = inventorytutorial;
     }
 
     public void OnCloseDialogueButton()
@@ -43,5 +41,10 @@ public class DialogueMenu : MonoBehaviour
         
         HUD.dialogueMenu.SetActive(false);
         HUD.inGameHUD.SetActive(true);
+    }
+
+    public void OnCloseInventoryTutorialButton()
+    {
+        inventoryTutorialMenu.SetActive(false);
     }
 }
